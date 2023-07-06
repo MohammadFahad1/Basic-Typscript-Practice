7d354cd8bc0e638343c379fed71911682d27bb3b
MohammadFahad1 <fahad4bangladesh@gmail.com>
2023-07-06 12:23:50 +0600

    Type Assertion

--- src/index.ts
+++ src/index.ts
@@ -13,7 +13,13 @@ const isAdmit: boolean = true;
 
 // Touple types
 const couple: [string, string] = ['Md. Fahad Monshi', 'Mrs. Fahad'];
-const status: (string | boolean)[] = ['Md. Fahad Monshi', true];
+const Status: (string | boolean)[] = ['Md. Fahad Monshi', true];
 const rollNums: number[] = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20];
 
-console.log(course);
+function myFriends(...friends: string[]) {
+   friends.map(friend => console.log(friend));
+}
+
+// myFriends('Fahad', 'Shourav', 'Porshi', 'Sweety')
+
+// console.log(course);
