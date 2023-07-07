bc2fdeb4b11937bf50a2cc39cfcfe5897bca3187
MohammadFahad1 <fahad4bangladesh@gmail.com>
2023-07-07 09:20:08 +0600

    Generic Keyof Constraints

---
+++ src/module3/generic-keyof-constraints.ts
@@ -0,0 +1,18 @@
+type PersonType = {
+    name: string;
+    age: number;
+    address: string;
+}
+
+type newType = "name" | "age" | "address"; //manually korsi
+
+type newTypeUsingKeyOff = keyof PersonType;
+
+const a: newType = 'name';
+const b: newTypeUsingKeyOff = 'address';
+
+function getProperty<X, Y extends keyof X>(obj: X, key: Y) {
+    obj[key]
+}
+
+const property = getProperty({ name: 'Mr. X', age: 100 }, "age");
