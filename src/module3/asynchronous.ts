17c4b824fbf56a646dd2bbbff99660cb3ef89660
Office PC ABA (Fahad) <abasc1962@gmail.com>
2023-07-08 11:49:57 +0600

    Asynchronous

--- src/module3/asynchronous.ts
+++ src/module3/asynchronous.ts
@@ -1,4 +1,25 @@
 // Moking
+// Json Placeholder
+
+interface ITodo {
+    userId: number,
+    id: number,
+    title: string,
+    completed: boolean
+}
+
+const getTodo = async (): Promise<ITodo> => {
+    const response = await fetch('https://jsonplaceholder.typicode.com/todos/1');
+    return await response.json();
+}
+
+const getTodoData = async (): Promise<void> => {
+    const result = await getTodo();
+    console.log(result);
+}
+
+getTodoData();
+
 const makePromise = (): Promise<string> => {
     return new Promise<string>((resolve, reject) => {
         const data: string = 'Data is Fetched';
