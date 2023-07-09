d832c9995cb9848f3c77428085ce35db428d376e
Office PC ABA (Fahad) <abasc1962@gmail.com>
2023-07-09 18:15:41 +0600

    Little Practice

--- src/module3/asynchronous.ts
+++ src/module3/asynchronous.ts
@@ -1,38 +1,38 @@
-// Moking
-// Json Placeholder
+// // Moking
+// // Json Placeholder
 
-interface ITodo {
-    userId: number,
-    id: number,
-    title: string,
-    completed: boolean
-}
+// interface ITodo {
+//     userId: number,
+//     id: number,
+//     title: string,
+//     completed: boolean
+// }
 
-const getTodo = async (): Promise<ITodo> => {
-    const response = await fetch('https://jsonplaceholder.typicode.com/todos/1');
-    return await response.json();
-}
+// const getTodo = async (): Promise<ITodo> => {
+//     const response = await fetch('https://jsonplaceholder.typicode.com/todos/1');
+//     return await response.json();
+// }
 
-const getTodoData = async (): Promise<void> => {
-    const result = await getTodo();
-    console.log(result);
-}
+// const getTodoData = async (): Promise<void> => {
+//     const result = await getTodo();
+//     console.log(result);
+// }
 
-getTodoData();
+// getTodoData();
 
-const makePromise = (): Promise<string> => {
-    return new Promise<string>((resolve, reject) => {
-        const data: string = 'Data is Fetched';
-        if (data) {
-            resolve(data)
-        } else {
-            reject('Failed to Fetch Data!');
-        }
-    })
-}
+// const makePromise = (): Promise<string> => {
+//     return new Promise<string>((resolve, reject) => {
+//         const data: string = 'Data is Fetched';
+//         if (data) {
+//             resolve(data)
+//         } else {
+//             reject('Failed to Fetch Data!');
+//         }
+//     })
+// }
 
-const getPromiseData = async (): Promise<string> => {
-    const data = await makePromise();
-    return data;
-}
-// Promise<string> Promise<boolean> Promise<object>
+// const getPromiseData = async (): Promise<string> => {
+//     const data = await makePromise();
+//     return data;
+// }
+// // Promise<string> Promise<boolean> Promise<object>
---
+++ src/module3/mapped-types.ts
@@ -0,0 +1,98 @@
+// const arrayOfNumbers = [1, 2, 3]; //['1', '2', '3']
+// const arrayOfStrings = arrayOfNumbers.map((number) => number.toString());
+// // console.log(arrayOfStrings);
+
+// type AreaNumber = {
+//     height: number;
+//     width: number
+// }
+
+// type AreaString = {
+//     height: string;
+//     width: string;
+// }
+
+// type AreaReadOnly = {
+//     readonly height: number;
+//     readonly width: number
+// }
+
+// const rectangularArea: AreaReadOnly = {
+//     height: 10,
+//     width: 12
+// }
+
+// type A = AreaNumber['height'] //look up types
+// type B = keyof AreaNumber;  //'width' | 'height'
+
+
+// type User = {
+//     name: string;
+//     age: number;
+// }
+
+// type UserExtendedType = User & {
+//     role: string
+// }
+
+// interface IUser {
+//     name: string;
+//     age: number;
+// }
+
+
+
+// interface IExtendedUser extends IUser {
+//     role: string
+// }
+
+// const userWithTypeAlias: UserExtendedType = {
+//     name: "Md. Fahad Monshi",
+//     age: 24,
+//     role: "Regular User"
+// }
+
+// const userwithInterface: IExtendedUser = {
+//     name: "Md. Fahad Monshi",
+//     age: 24,
+//     role: "Admin"
+// }
+
+// type RollNumber = number;
+// // Object, Function, Array
+
+// interface IAddNumbersType {
+//     (num1: number, num2: number): number;
+// }
+
+// const addNumbers: IAddNumbersType = (num1, num2) => {
+//     return num1 + num2;
+// }
+
+// interface RollNumberType {
+//     [index: number]: number
+// }
+// const rollNumber: RollNumberType = [1, 2, 3, 4, 5];
+
+interface IRelationWithSalary {
+    husband: string;
+    wife: string;
+}
+
+interface RelationWithSalary<T, S> {
+    name: T;
+    salary: S;
+}
+
+const couple: RelationWithSalary<string, number> = {
+    name: "Md. Fahad Monshi",
+    salary: 1000000
+}
+
+const touple: RelationWithSalary<IRelationWithSalary, number> = {
+    name: {
+        husband: "Md. Fahad Monshi",
+        wife: 'Future Wife'
+    },
+    salary: 10000
+}
