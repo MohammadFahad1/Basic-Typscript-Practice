31d2c1d33b014e50607810cce56c0954b76177b3
MohammadFahad1 <fahad4bangladesh@gmail.com>
2023-07-17 22:27:26 +0600

    done

--- src/module4/abstraction.ts
+++ src/module4/abstraction.ts
@@ -8,28 +8,51 @@
 //     model: '2000'
 // }
 
-interface IVehicle {
-    startEngine(): void;
-    stopEngine(): void;
-    move(): void;
-}
+// interface Vehicle {
+//     startEngine(): void;
+//     stopEngine(): void;
+    // move?(): void;
+// }
 
-class vehicle implements IVehicle {
-    constructor(public name: string, public brand: string, public model: number) {
+// class Car implements Vehicle {
+//     constructor(public name: string, public brand: string, public model: number) {
 
-    }
-    startEngine(): void {
-        console.log(`I am starting the engine`);
-    }
-    stopEngine(): void {
-        console.log(`I am stopping the engine`);
-    }
-    move(): void {
-        console.log(`I am moving the engine`);
-    }
-    test() {
-        console.log(`I am for testing purpose!`);
-    }
-}
+//     }
+//     startEngine(): void {
+//         console.log(`I am starting the engine`);
+//     }
+//     stopEngine(): void {
+//         console.log(`I am stopping the engine`);
+//     }
+//     move(): void {
+//         console.log(`I am moving the engine`);
+//     }
+//     test() {
+//         console.log(`I am for testing purpose!`);
+//     }
+// }
 
-const vehicle1 = new vehicle("Car", "Toyota", 200000);
+// const vehicle1 = new Car("Car", "Toyota", 200000);
+
+// abstract class
+// abstract class Vehicle {
+//     constructor(public name: string, public brand: string, public model: number) {
+
+//     }
+//     abstract startEngine(): void
+//     abstract stopEngine(): void
+//     move(): void {
+//         console.log(`I am moving the engine`);
+//     }
+// }
+
+// // const car1 = new Vehicle("Car", "Honda", 2015);
+// class Car extends Vehicle {
+//     startEngine(): void {
+//         console.log(`I am starting engine..`);
+//     }
+//     stopEngine(): void {
+//         console.log(`I am stopping engine..`);
+//     }
+// }
+
